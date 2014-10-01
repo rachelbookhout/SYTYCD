894a307d8af5298302a51f4804aa1b6595a9d9b6
rachelbookhout <rbookhou@gmail.com>
2014-09-30 21:43:31 -0400

    Working pull for both partner and dancer but no working view

--- app/controllers/dancers_controller.rb
+++ app/controllers/dancers_controller.rb
@@ -14,14 +14,13 @@ class DancersController < ApplicationController
 
   def show
   id = params[:id].to_i
-  @dances = Performance.where({dancer_id: id || partner_id :id})
+  @dances = []
+  @dances << Performance.where(dancer_id: id)
+  @dances << Performance.where(partner_id: id)
   @dance_videos = @dances.each do |dance|
-  get_videos(dance.name,dance.partner,dance.song)
-  dance.url = @url
-  dance.video_id = @url.match(/watch\?v=(.*?)(?:&feature|\Z)/)[1]
+  get_videos(dance[0]["dancer_name"],dance[0]["partner"],dance[0]["song"])
+  dance[0]["url"] = @url
+  dance[0]["video_id"] = @url.match(/watch\?v=(.*?)(?:&feature|\Z)/)[1]
   end
   end
-
-
-
 end
