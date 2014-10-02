18d6d81b6f1b37c89b03b250f2bd01d06e25b4d0
rachelbookhout <rbookhou@gmail.com>
2014-10-02 09:08:44 -0400

    Trying to get the csv to work

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,9 +2,9 @@
 ## Option 2:Use the CSV
 
 require 'csv'
-CSV.foreach('dancer.csv', :headers => true) do |row|
+CSV.foreach('#{Rails.root}/db/dancer.csv', :headers => true) do |row|
   Dancer.create!(row.to_hash)
 end
-CSV.foreach('performance.csv', :headers => true) do |row|
+CSV.foreach('#{Rails.root}/db/performance.csv', :headers => true) do |row|
   Performance.create!(row.to_hash)
 end
