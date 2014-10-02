d767d2ba839b0a1d1955a70fb571d54f043c24c5
rachelbookhout <rbookhou@gmail.com>
2014-10-02 11:12:56 -0400

    Retry

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,10 +1,10 @@
 ## Option 1: Use the Dancer Data and input into your Rails Console
 ## Option 2:Use the CSV
 
-require 'csv'
-CSV.foreach('#{Rails.root}/db/dancer.csv', :headers => true) do |row|
-  Dancer.create!(row.to_hash)
-end
-CSV.foreach('#{Rails.root}/db/performance.csv', :headers => true) do |row|
-  Performance.create!(row.to_hash)
-end
+# require 'csv'
+# CSV.foreach('#{Rails.root}/db/dancer.csv', :headers => true) do |row|
+#   Dancer.create!(row.to_hash)
+# end
+# CSV.foreach('#{Rails.root}/db/performance.csv', :headers => true) do |row|
+#   Performance.create!(row.to_hash)
+# end
